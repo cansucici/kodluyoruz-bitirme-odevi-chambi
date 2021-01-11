e72c3f95cde545bc53fe0958b7941968e02019dc
isilacar <isilacar@hotmail.com.tr>
2021-01-11 19:58:10 +0300

    service package created

---
+++ library/src/main/java/org/kodluyoruz/group1/library/service/AuthorService.java
@@ -0,0 +1,9 @@
+package org.kodluyoruz.group1.library.service;
+
+import org.kodluyoruz.group1.library.dto.AuthorDTO;
+import org.kodluyoruz.group1.library.model.entities.Authors;
+
+public interface AuthorService {
+
+    Authors save(AuthorDTO authorDTO);
+}
