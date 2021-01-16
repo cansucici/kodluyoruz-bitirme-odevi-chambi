ece704968723ceb510a2d9b411da054dfe85bd44
isilacar <isilacar@hotmail.com.tr>
2021-01-16 22:22:59 +0300

    authors added

---
+++ library/src/main/java/org/kodluyoruz/group1/library/control/AuthorController.java
@@ -0,0 +1,47 @@
+
+package org.kodluyoruz.group1.library.control;
+
+import lombok.RequiredArgsConstructor;
+import org.kodluyoruz.group1.library.dto.AuthorDTO;
+import org.kodluyoruz.group1.library.model.entities.Authors;
+import org.kodluyoruz.group1.library.service.AuthorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/authors")
+@RequiredArgsConstructor
+public class AuthorController {
+
+    private final AuthorService service;
+
+    @PostMapping("/new")
+    public Authors save(@RequestBody AuthorDTO dto){
+        Authors author=service.saveAuthor(dto);
+        return author;
+    }
+
+    @GetMapping
+    public Collection<Authors> getAllActiveAuthors(){
+      return service.getAllActive();
+    }
+
+    @GetMapping("/{name}")
+    public Collection<Authors> getAllAuthorsByName(@PathVariable String name){
+        return service.findByNameSurname(name);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAuthor(@PathVariable Long id){
+        service.deleteById(id);
+    }
+
+    @PutMapping("/update")
+    public Authors updateAuthors(@RequestBody AuthorDTO dto){
+        Authors authors=service.updateAuthor(dto);
+        return authors;
+    }
+
+}
---
+++ library/src/main/java/org/kodluyoruz/group1/library/dao/AuthorRepository.java
@@ -0,0 +1,28 @@
+package org.kodluyoruz.group1.library.dao;
+
+import org.kodluyoruz.group1.library.dto.AuthorDTO;
+import org.kodluyoruz.group1.library.model.entities.Authors;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.List;
+
+
+public interface AuthorRepository extends JpaRepository<Authors, Long> {
+
+    @Query("select a from Authors a where a.deleted=false")
+    List<Authors> findAll();
+
+    @Query("select a from Authors a where a.deleted=false and a.nameSurname=:name")
+    Collection<Authors> findAllByNameSurname(String name);
+
+    @Modifying
+    @Transactional
+    @Query("update Authors a set a.deleted=true where a.id=:id")
+    void updateAuthorStatus(Long id);
+
+
+}
