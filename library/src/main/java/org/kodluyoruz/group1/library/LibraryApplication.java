a97a7adef8de364dec0dfb8ed837546117f161b2
isilacar <isilacar@hotmail.com.tr>
2021-01-17 14:14:37 +0300

    deneme

--- library/src/main/java/org/kodluyoruz/group1/library/LibraryApplication.java
+++ library/src/main/java/org/kodluyoruz/group1/library/LibraryApplication.java
@@ -4,7 +4,7 @@ import org.kodluyoruz.group1.library.dao.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+//
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = MemberRepository.class)
 public class LibraryApplication {
