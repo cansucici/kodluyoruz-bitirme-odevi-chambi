9ae5fb2a480046251f013986a0cdcaebabd6e588
isilacar <isilacar@hotmail.com.tr>
2021-01-23 17:40:15 +0300

    hghgj

--- library/src/main/java/org/kodluyoruz/group1/library/model/entities/BaseEntity.java
+++ library/src/main/java/org/kodluyoruz/group1/library/model/entities/BaseEntity.java
@@ -4,9 +4,6 @@ package org.kodluyoruz.group1.library.model.entities;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-
 import javax.persistence.*;
 import java.util.Date;
 
