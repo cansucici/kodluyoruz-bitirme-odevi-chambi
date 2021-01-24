b803e39d77574bc403711228ad797bb49accf12a
cansucici <cansu.cici.1@gmail.com>
2021-01-24 14:52:13 +0300

    change frontend

--- library/src/main/java/org/kodluyoruz/group1/library/control/BookController.java
+++ library/src/main/java/org/kodluyoruz/group1/library/control/BookController.java
@@ -4,15 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.kodluyoruz.group1.library.dto.BookDTO;
 import org.kodluyoruz.group1.library.model.entities.Book;
 import org.kodluyoruz.group1.library.service.BookService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/books")
 public class BookController {
 
     //endpointler düzenlendi.
@@ -23,21 +23,26 @@ public class BookController {
 
     @GetMapping("/booklist")
     public String getBookList(Model model) {
-        model.addAttribute("booklist", bookService.getAllBooks());
+        model.addAttribute("listbook", bookService.getAllBooks());
         return "book_list";
     }
 
+    @GetMapping("/saveBook")
+    public String getCreateBook(Model model) {
+        model.addAttribute("dtoBook", new BookDTO());
+        return "add_book";
+    }
 
     @PostMapping("/saveBook")
-    public String saveBook(@ModelAttribute("book") @Validated BookDTO bookDTO) {
-       bookService.save(bookDTO);
-return "redirect:/booklist";
+    public String postCreateBook(@Validated BookDTO bookDTO) {
+        bookService.save(bookDTO);
+        return "redirect:/book_list";
     }
 
     @PutMapping("/showBookFormForUpdate/{id}")
     public String updateBook(@ModelAttribute @Validated BookDTO bookDTO) {
-         bookService.update(bookDTO);
-        return  "redirect:/booklist";
+        bookService.update(bookDTO);
+        return "redirect:/booklist";
     }
 
     @DeleteMapping("/delete/{id}")
@@ -46,7 +51,7 @@ return "redirect:/booklist";
     }
 
 
-    @GetMapping("/{bookName}")
+    @GetMapping("/books/{bookName}")
     public Collection<Book> showSearchResult(@PathVariable String bookName) {
         return bookService.getBooksByBookName(bookName);
     }
