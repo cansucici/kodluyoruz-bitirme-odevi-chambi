c55870eab6262a7ee57ce75c8279fa4b0376bcf7
isilacar <isilacar@hotmail.com.tr>
2021-01-25 17:26:16 +0300

    author ve book sınıflarındaki alanlara -doldurulması zorunlu- mesajı eklendi.Projenin genelinde  Türkçeleştirme yapıldı.

--- library/src/main/java/org/kodluyoruz/group1/library/control/AuthorController.java
+++ library/src/main/java/org/kodluyoruz/group1/library/control/AuthorController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+
 @Controller
 @RequiredArgsConstructor
 public class AuthorController {
--- library/src/main/java/org/kodluyoruz/group1/library/dto/AuthorDTO.java
+++ library/src/main/java/org/kodluyoruz/group1/library/dto/AuthorDTO.java
@@ -3,6 +3,7 @@ package org.kodluyoruz.group1.library.dto;
 import java.util.List;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import org.kodluyoruz.group1.library.model.entities.Book;
 
@@ -18,9 +19,9 @@ import lombok.Setter;
 public class AuthorDTO extends BaseDTO {
 
     @NotEmpty(message="isim soyisim alanı boş olamaz.")
+    @NotNull(message="isim soyisim alanı boş olamaz. ")
     private String nameSurname;
 
-    
     private String about;
 
     private List<Book> books;
