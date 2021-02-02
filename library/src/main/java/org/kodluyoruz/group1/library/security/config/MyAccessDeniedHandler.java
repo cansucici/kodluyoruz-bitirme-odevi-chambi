27058f387de296ae730708528672275b3b03eac5
Beyza Acar <beyzaacar4141@gmail.com>
2021-02-02 04:50:04 +0300

    SecurityConfiguration fix, ErrorController added, MemberDetails added authorities, member service fix

--- library/src/main/java/org/kodluyoruz/group1/library/security/config/MyAccessDeniedHandler.java
+++ library/src/main/java/org/kodluyoruz/group1/library/security/config/MyAccessDeniedHandler.java
@@ -5,13 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 
+@Component
 public class MyAccessDeniedHandler implements AccessDeniedHandler {
 
     private static Logger logger = LoggerFactory.getLogger(MyAccessDeniedHandler.class);
